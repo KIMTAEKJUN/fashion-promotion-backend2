44b131738f66c1cfe696a0c971938e26f4b86595
Kuah_0 <gdsygrt557422@naver.com>
2024-08-12 18:39:03 +0900

    style: file name modify

---
+++ src/user/entities/users.entity.ts
@@ -0,0 +1,31 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('User')
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255, nullable: false, unique: true })
+  email: string;
+
+  @Column({ type: 'varchar', length: 20, nullable: false })
+  userName: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  profileImage: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  socialId: string;
+
+  @Column({ type: 'enum', enum: ['KAKAO', 'GOOGLE'], nullable: true })
+  socialType: string;
+
+  @Column({ type: 'enum', enum: ['USER', 'ADMIN'], nullable: true })
+  Role: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
